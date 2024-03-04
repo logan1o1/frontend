bce65370f11893717703f203c023486d974ebb65
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-03-04 21:52:44 +0530

    sell page

--- src/components/Sell/Sell.jsx
+++ src/components/Sell/Sell.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { TextField } from "@mui/material";
+import React from "react";
 
 const Sell = () => {
   return (
-    <div>Sell</div>
-  )
-}
+    <div>
+      <h1>Sell</h1>
+      <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </div>
+    </div>
+  );
+};
 
-export default Sell
+export default Sell;
