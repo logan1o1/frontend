7d206a047fa70294cae48c462f0d2f23aad6056f
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-04 22:59:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import store from './redux/store.js';
 import './index.css'
 
-import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
