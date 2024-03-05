247f06ccd74b2652e5dae5375c6ae5a88e842450
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2024-03-05 10:28:10 +0530

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Cards from "../../components/Cards";
-import Nav from "../../components/Nav";
 
 const Home = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const getList = async () => {
+      try {
+        const res = await fetch("http://localhost:8888/user/get-listed", {
+          method: "GET"
+        });
+        const data = await res.json();
+        setBooks(data)
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getList()
+  }, [])
   
+
   return (
     <>
       <div className="container">
         <div className="cards-container">
+          {books && books.map((book) => {})}
           <Cards />
         </div>
       </div>
